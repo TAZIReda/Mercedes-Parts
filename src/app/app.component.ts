cdaa51f0b9f4459bc220c1eef51c80705c5211ec
TAZIReda <tazir_reda@outlook.fr>
2023-12-24 14:43:05 +0100

    Set up the Frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeIcons,MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', 
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'frontend';
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+      this.items = [
+          {
+              label: 'Update',
+              icon: 'pi pi-refresh'
+          },
+          {
+              label: 'Delete',
+              icon: 'pi pi-times'
+          },
+          {
+              label: 'Angular',
+              icon: 'pi pi-external-link',
+              url: 'http://angular.io'
+          },
+          {
+              label: 'Router',
+              icon: 'pi pi-upload',
+              routerLink: '/fileupload'
+          }
+      ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarModule } from 'primeng/toolbar';
+import{ButtonModule} from 'primeng/button';
+import {SplitButtonModule} from 'primeng/splitbutton';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToolbarModule,
+    ButtonModule,
+    SplitButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
